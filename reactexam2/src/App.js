28aae79fac4b0d0b1bb4d227fe9be55238c88ef9
Chedda98 <twa05157@gmail.com>
2022-05-03 01:38:45 +0900

    Feat: 이름과 닉네임 입력하는 Info.js 파일 생성

--- reactexam2/src/App.js
+++ reactexam2/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Counter from './Counter';
+import Info from './Info';
 // import "./App.css";
 
 // import MyHeader from './MyHeader';
 // import MyFooter from "./MyFooter";
 
 const App = () => {
-  return <Counter />
+  return <Info />;
+  
 }
 
 export default App;
